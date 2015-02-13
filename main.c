68335ff75a52d87cc3693a552df6911504a272f2
Nicolò Vescera <slayercandy@icloud.com>
2015-02-13 17:14:49 +0100

    Update main.c && Perso Saltapercossa

--- main.c
+++ main.c
@@ -17,15 +17,22 @@ int main(int argc, char** argv) {
     rRicorsivo = potenzaIterativa(base, esponente);
     
     printf("%d^%d = %d (formulazione iterativa)\n", base, esponente, rIterativo);
-    printf("%d^%d = %d (formulazione iterativa)\n", base, esponente, rRicorsivo);
+    printf("%d^%d = %d (formulazione ricorsiva)\n", base, esponente, rRicorsivo);
     return (EXIT_SUCCESS);
 }
 
 int potenzaIterativa(int base, int esponente) {
-    // TODO Implementa il corpo della funzione
-    return -1;
+    int r=1;
+    for(esponente;esponente>0;esponente--)
+        r*=base;
+    return r;
 }
-int potenzaIterativa(int base, int esponente) {
-    // TODO Implementa il corpo della funzione
-    return -1;
+int potenzaRicorsiva(int base, int esponente) {//corretto il nome della funzione, ha perso un bonus saltapercossa!
+    int r=0;
+    if(esponente==0)
+        return 1;
+    else{
+        r=base*potenzaRicorsiva(base,esponente-1);
+        return r;
+    }
 }
